286bbacc9f208b55b692de179cbc964583ec11dc
Thorn <stachelrodtr4@gmail.com>
2020-09-14 19:24:52 -0400

    need to commit for checkout. pls ignore

--- src/data/Data.js
+++ src/data/Data.js
@@ -2,13 +2,17 @@ const Data = [
     {
         "question": "1",
         "title": "What Kind of Rioter Are You?",
+        "a": "Ellie, the Enlightened",
+        "b": "Colin, the Punk",
+        "c": "Kylie Jo, the Tomboy",
+        "d": "Janet, the Funky",
         "q1": "You\'re at a movie theater, and snacks are outragously expensive. What do you do?",
         "q1Answers": {
             "a1": "Buy the snacks begrudingly",
             "a1Value": "a0b4c2d3",
             "a2": "Sneak in your own food",
             "a2Value": "a4b2c0d2",
-            "a3": "Make your friend buy it",
+            "a3": "Make your friend buy the snacks",
             "a3Value": "a3b2c4d2",
             "a4": "Run a campaign against the hyperinflation of popcorn. The people deserve to know!",
             "a4Value": "a0b0c0d4"
@@ -17,33 +21,101 @@ const Data = [
         "q2Answers": {
             "a1": "FunkyTown",
             "a1Value": "a0b0c0d4",
-            "a2": "Iceland",
+            "a2": "Ireland",
             "a2Value": "a0b2c4d1",
-            "a3": "placeholder",
+            "a3": "Norway",
             "a3Value": "a0b4c0d0",
+            "a4": "Italy",
+            "a4Value": ""
+        },
+        "q3": "If you could pick your dream vehicle, what would it be?",
+        "q3Answers": {
+            "a1": "Volkswagen Thing",
+            "a1Value": "",
+            "a2": "Mini Cooper",
+            "a2Value": "",
+            "a3": "\'77 Ford Pinto",
+            "a3Value": "",
+            "a4": "placeholder",
+            "a4Value": ""
+        },
+        "q4": "What TV Show do you most relate to?",
+        "q4Answers": {
+            "a1": "Primeval (BBC)",
+            "a1Value": "",
+            "a2": "Working Moms",
+            "a2Value": "",
+            "a3": "Avatar The Last Airbender",
+            "a3Value": "",
             "a4": "",
             "a4Value": ""
         },
-        "q3": "",
-        "q3Answers": {},
-        "q4": "",
-        "q4Answers": {},
-        "q5": "",
-        "q5Answers": {},
+        "q5": "What animal best describes you?",
+        "q5Answers": {
+            "a1": "Cardinal (bird)",
+            "a1Value": "",
+            "a2": "Grizzly Bear",
+            "a2Value": "",
+            "a3": "",
+            "a3Value": "",
+            "a4": "Panda",
+            "a4Value": ""
+        },
         "q6": "",
-        "q6Answers": {},
+        "q6Answers": {
+            "a1": "",
+            "a1Value": "",
+            "a2": "",
+            "a2Value": "",
+            "a3": "",
+            "a3Value": "",
+            "a4": "",
+            "a4Value": ""
+        },
         "q7": "",
-        "q7Answers": {},
+        "q7Answers": {
+            "a1": "",
+            "a1Value": "",
+            "a2": "",
+            "a2Value": "",
+            "a3": "",
+            "a3Value": "",
+            "a4": "",
+            "a4Value": ""
+        },
         "q8": "",
-        "q8Answers": {},
+        "q8Answers": {
+            "a1": "",
+            "a1Value": "",
+            "a2": "",
+            "a2Value": "",
+            "a3": "",
+            "a3Value": "",
+            "a4": "",
+            "a4Value": ""
+        },
         "q9": "",
-        "q9Answers": {},
+        "q9Answers": {
+            "a1": "",
+            "a1Value": "",
+            "a2": "",
+            "a2Value": "",
+            "a3": "",
+            "a3Value": "",
+            "a4": "",
+            "a4Value": ""
+        },
         "q10": "",
-        "q10Answers": {},
-        "a": "Ellie, the Enlightened",
-        "b": "Colin, the Punk",
-        "c": "Kylie Jo, the Tomboy",
-        "d": "Janet, the Funky"
+        "q10Answers": {
+            "a1": "",
+            "a1Value": "",
+            "a2": "",
+            "a2Value": "",
+            "a3": "",
+            "a3Value": "",
+            "a4": "",
+            "a4Value": ""
+        }
     },
     {}
 ]
