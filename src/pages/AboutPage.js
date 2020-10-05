14d324431403b0a8e3ca4d12c45ae0eed31dc454
Janet Cook <monickercoding@gmail.com>
2020-10-04 19:41:16 -0600

    latest version of aboutpage branch updates

--- src/pages/AboutPage.js
+++ src/pages/AboutPage.js
@@ -1,18 +1,45 @@
 import React from "react";
 import Menu from "../components/menu/Menu";
 import "./AboutPage.module.css";
+import { userIsAuthenticated } from "../redux/HOCs"
 
 class About extends React.Component {
     render() {
         return (
-            <section>
+            <div>
                 <Menu isAuthenticated={this.props.isAuthenticated} />
 
-                <div id="AboutRDR"></div>
+                <section id="images" class="page-row">
 
-                    <h1>WHO WE ARE!</h1>
-                    <p>
-                        We’re <strong>RIOTERS REINVENTED</strong>. We’re not your run-of-the-mill developers.
+                    <div class="image" id="PunkDuck">
+                        <h3>Colin Stachelrodt</h3>
+                        <a href="">Punk Duckie Rioter</a>
+                    </div>
+
+                    <div class="image" id="ZenDuckie">
+                        <h3>Elisua Hernandez</h3>
+                        <a href="../assets/images/Zenduckie.png">Zen Duckie Rioter</a>
+                    </div>
+
+                    <div class="image" id="FunkyDuck">
+                        <h3>Janet Cook</h3>
+                        <a href="../assets/images/Funkyduck.jpg">Funky Duckie Rioter</a>
+                    </div>
+
+                    <div class="image" id="IrishDuckie">
+                        <h3>Kylie Jo McCafferty</h3>
+                        <a href="../assets/images/Irishduckie.jpg">Irish Duckie Rioter</a>
+                    </div>
+
+                </section>
+
+            
+
+                < div id="AboutRDR" ></div >
+
+                <h1>WHO WE ARE!</h1>
+                <p>
+                    We’re <strong>RIOTERS REINVENTED</strong>. We’re not your run-of-the-mill developers.
                         <br></br>
                         We take rioting and make it our own. We mold breakers.
                         <br></br>
@@ -20,31 +47,12 @@ class About extends React.Component {
                         <br></br>
                         As code innovaters we invite you to join the Riot!
                         <br></br>
-                    </p>
-                    <h2>"We're not bad ducks, We're badass!"</h2>
-                
-
-
-
-                <div className="Gallery">
-                    {/* <img src="../assets/images/Punkduckie.jpg">Punk Duckie Rioter</img>
-                        <div className="desc"> Colin Stachelrodt </div> 
-                    <img src="../assets/images/Zenduckie.png">Zen Duckie Rioter</img>
-                        <div className="desc"> Elisua Hernandez </div>
-                    <img src="../assets/images/Funkyduck.jpg">Funky Duckie Rioter</img>
-                        <div className="desc"> Janet Cook </div>
-                    <img src="../assets/images/Irishduckie.jpg">Irish Duckie Rioter</img>
-                        <div className="desc"> Kylie Jo McCafferty </div> */}
-                </div>
-
-            </section>
-
-
-
-
+                </p>
+                <h2>"We're not bad ducks, We're badass!"</h2>
+         </div>            
 
         )
     }
 }
 
-export default About 
+export default userIsAuthenticated(About) 
